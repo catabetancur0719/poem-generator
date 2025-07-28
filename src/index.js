2aca4c1f06e46535a22233da1246893a6e8ee941
catabetancur0719 <catalina.betancur0719@gmail.com>
2025-07-28 12:05:46 -0500

    fixed js and typewritter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 function displayPoem(response) {
-  new Typewriter("#poem-output", {
-    strings: [response.data.answer],
+  const poemOutput = document.querySelector("#poem-output");
+  poemOutput.innerHTML = ""; // limpia lo anterior
+
+  const typewriter = new Typewriter(poemOutput, {
     autoStart: true,
-    delay: 1,
+    delay: 100,
     cursor: "",
   });
+
+  typewriter.typeString(response.data.answer).start();
+  console.log(response.data.answer);
 }
 
 function generatePoem(event) {
+  event.preventDefault();
 
 
   const instructionInput = document.querySelector("#user-instructions");
   const apiKey = "fb3o96aeef26e064f124eb8cta459256";
   const prompt = `user instructions: generate latin poems ${instructionInput.value}`;
   const context =
-    "Eres un colombiano experto en dichos, por favor crea un dicho en español, que sea maximo de 5 renglones y con la palabra que te da el usuario";
+    "You are a Colombian expert in creating popular poems and sayings. Write a short saying or poem in Spanish that has a wise, funny, or thoughtful tone. It must contain the word the user gives you, be a maximum of 5 lines long.";
   const apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(displayPoem);
